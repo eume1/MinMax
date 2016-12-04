1d325cbe3a7759570c897e7f811f4ad8356f58a2
e-ume <umeemeka@gmail.com>
2016-12-03 20:44:02 -0500

    new file:   .idea/uiDesigner.xml 	modified:   .idea/workspace.xml 	deleted:    out/production/context relevant/MeanMax.class 	deleted:    out/production/context relevant/technical.class 	modified:   src/main/java/MeanMax.java 	modified:   src/main/java/technical.java 	modified:   target/resolution-cache/default/context-relevant$sbt_2.12/1.0/resolved.xml.properties 	modified:   target/resolution-cache/default/context-relevant$sbt_2.12/1.0/resolved.xml.xml 	modified:   target/resolution-cache/default/context-relevant$sources_2.12/1.0/resolved.xml.properties 	modified:   target/resolution-cache/default/context-relevant$sources_2.12/1.0/resolved.xml.xml 	modified:   target/resolution-cache/default/context-relevant$sources_javadoc_2.12/1.0/resolved.xml.properties 	modified:   target/resolution-cache/default/context-relevant$sources_javadoc_2.12/1.0/resolved.xml.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sbt_2.12-default.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_2.12-default.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-compile-internal.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-compile.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-docs.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-optional.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-plugin.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-pom.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-provided.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-runtime-internal.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-runtime.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-scala-tool.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-sources.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-test-internal.xml 	modified:   target/resolution-cache/reports/default-context-relevant$sources_javadoc_2.12-test.xml 	modified:   target/streams/$global/updateClassifiers/$global/streams/out 	modified:   target/streams/$global/updateSbtClassifiers/$global/streams/out

--- src/main/java/MeanMax.java
+++ src/main/java/MeanMax.java
@@ -4,6 +4,7 @@
 public class MeanMax<A, B> {
     public final A mean;
     public final B max;
+
     public MeanMax(A mean, B max) {
         this.mean = mean;
         this.max = max;
--- src/main/java/technical.java
+++ src/main/java/technical.java
@@ -5,13 +5,15 @@ import java.util.*;
  */
 public class technical {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-        List<Object> obj =new ArrayList<>();
-        for (int i=0; i < 8; i++){
+        int windowSize = 4; //configurable
+
+        List<Object> obj = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
             obj.add(null);
         }
-        //System.out.println("OBJECT: " + obj.size());
+
         obj.add(2); obj.add(3); obj.add(null); obj.add(null);
         obj.add(3); obj.add(4); obj.add(null); obj.add(null);
         obj.add(4); obj.add(5); obj.add(3); obj.add("blah");
@@ -19,41 +21,38 @@ public class technical {
 
         // A reversed list stimulates a LIFO stream
         Collections.reverse(obj);
-        generateMeanMax(5,obj);
+        generateMeanMax(windowSize, obj);
     }
 
-    public static MeanMax generateMeanMax(int windowSize, List<Object> stream){
+    public static MeanMax generateMeanMax(int windowSize, List<Object> stream) {
         Set<Integer> subList = new LinkedHashSet<>();
         Object item;
 
-        for (int i = 0; i < stream.size(); i++){
+        for (int i = 0; i < stream.size(); i++) {
             item = stream.get(i);
-            if (subList.size()<windowSize && item!=null && item instanceof Number){
+            if (subList.size() < windowSize && item != null && item instanceof Number) {
                 subList.add(Integer.parseInt(item.toString()));
             }
         }
-        return compute(windowSize,subList);
+        return compute(windowSize, subList);
     }
 
-    public static MeanMax<Integer,Integer> compute(int windowSize, Set<Integer> set){
+    public static MeanMax<Integer, Integer> compute(int windowSize, Set<Integer> set) {
         int max = 0;
         int mean;
         int sum = 0;
-        MeanMax<Integer,Integer> mm;
+        MeanMax<Integer, Integer> mm;
 
-        System.out.println("SET_SIZE: " + set.size());
         int diff = windowSize - set.size();
-        System.out.println("DIFF: " + diff);
 
-        for (int i : set){
-            if(max < i) max=i;
-            System.out.println("i " + i);
-            sum+=i;
+        for (int i : set) {
+            if (max < i) max = i;
+            sum += i;
         }
 
-        mean = (sum+diff)/windowSize;
-        mm = new MeanMax<>(mean,max);
-        System.out.println("MEAN: " + mean + " MAX: " + max + " SET SIZE:" + set.size());
+        mean = (sum + diff) / windowSize;
+        mm = new MeanMax<>(mean, max);
+        System.out.println("MEAN: " + mean + " MAX: " + max + " SET SIZE: " + set.size());
         return mm;
     }
 
